a26b985fd6fd9c7ff1e234beb6a497478f6ff192
AdonadoMolinares <donadomolinares@gmail.com>
2025-11-24 10:27:02 -0500

    commit

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -29,7 +29,7 @@
 
     figure.mb-5
       .video.mb-2
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/JAUAw5CC4q4?si=MMRx_jCt-InS1nnz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/z7HI0LFOEkY?si=mAhWMA27IFT0av2_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video: Principios del <em>merchandising</em>
 
 </template>
